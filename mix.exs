db5ecd8132bb766f493ccebeef9f62f8750d167c
Dominic Letz <dominic.letz@berlin.de>
2025-05-09 13:53:40 +0200

    Added public_key to extra_apps

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Desktop.Deployment.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [extra_applications: [:crypto, :eex, :logger]]
+    [extra_applications: [:crypto, :eex, :logger, :public_key]]
   end
 
   defp aliases do
